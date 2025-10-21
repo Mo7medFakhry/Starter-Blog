18d9098af6155671f87bf33cfdfacc46ec52e966
Mo7medFakhry <mohamedhawas415@gmail.com>
2025-10-21 15:34:39 +0300

    theme&Auth

--- resources/views/theme/login.blade.php
+++ resources/views/theme/login.blade.php
@@ -11,17 +11,25 @@
         <div class="container">
             <div class="row">
                 <div class="col-6 mx-auto">
-                    <form action="#/" class="form-contact contact_form" action="contact_process.php" method="post"
-                        id="contactForm" novalidate="novalidate">
+                    <form action="{{ route('login') }}" class="form-contact contact_form" method="post"
+                        novalidate="novalidate">
+                        @csrf
                         <div class="form-group">
                             <input class="form-control border" name="email" id="email" type="email"
-                                placeholder="Enter email address">
+                                placeholder="Enter email address" :value="old('email')">
+                            <x-input-error :messages="$errors->get('email')" class="mt-2" />
                         </div>
                         <div class="form-group">
                             <input class="form-control border" name="password" id="name" type="password"
                                 placeholder="Enter your password">
+                            <x-input-error :messages="$errors->get('password')" class="mt-2" />
                         </div>
                         <div class="form-group text-center text-md-right mt-3">
+                            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                                href="{{ route('register') }}">
+                                {{ __('Sign up instead ?') }}
+                            </a>
+
                             <button type="submit" class="button button--active button-contactForm">Login</button>
                         </div>
                     </form>
@@ -32,5 +40,3 @@
     <!-- ================ contact section end ================= -->
 
 @endsection
-
-
--- routes/web.php
+++ routes/web.php
@@ -21,8 +21,6 @@ Route::controller(ThemeController::class)->name('theme.')->group(function () {
     Route::get('/category', 'category')->name('category');
     Route::get('/contact', 'contact')->name('contact');
     Route::get('/single-blog', 'singleBlog')->name('single-blog');
-    Route::get('/login' , 'login')->name('login');
-    Route::get('/register' , 'register')->name('register');
 });
 
 Route::get('/dashboard', function () {
